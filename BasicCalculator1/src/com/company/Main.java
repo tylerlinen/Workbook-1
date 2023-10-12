e8804074bbd3b3bb0affeed86a4d2b4d464d8a82
Tyler <tylerlinen@gmail.com>
2023-10-12 09:17:23 -0400

    added basic calculator1

---
+++ BasicCalculator1/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+        int first = myObj.nextInt();
+
+        System.out.println("Enter the second number");
+        int second = myObj.nextInt();
+
+        System.out.println("Multiply Subtract Add Divide");
+
+        String value = myObj.nextLine();
+        System.out.println(value);
+
+
+
+
+    }
+}
